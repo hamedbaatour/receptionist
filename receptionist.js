17780f198d6a3473075712bbef200742cb176e72
Hamed Baatour <hamedbaatour@gmail.com>
2020-02-15 16:42:10 +0300

    init(all): initial commit
    - setup native web component
    - HTML + CSS with some animations
    - basic open/close widget functionality

---
+++ receptionist.js
@@ -0,0 +1,38 @@
+// Create Receptionist (extending HTML Element)
+export class Receptionist extends HTMLElement {
+  // Fires when an instance of the element is created or updated
+  constructor() {
+    super();
+  }
+
+  // Fires when Receptionist gets inserted into the document
+  async connectedCallback() {
+    // Import component template (HTML)
+    this.attachShadow({ mode: "open" }).innerHTML = await (
+      await fetch("../receptionist.html")
+    ).text();
+
+    // Getting ShadowDOM elements
+    const fab = this.shadowRoot.querySelector(".fab");
+    const card = this.shadowRoot.querySelector(".card");
+
+    // FAB button click event handler
+    fab.addEventListener("click", () => {
+      card.classList.toggle("card-active");
+      // Switch animation between FAB closed text (Start Here !) and opened text ( X close )
+      [...fab.children].forEach(span => span.classList.toggle("show"));
+    });
+  }
+
+  // Fires when Receptionist gets removed from the document
+  disconnectedCallback() {}
+
+  // Fires when a Receptionist attribute is added, removed, or updated
+  attributeChangedCallback(attrName, oldVal, newVal) {}
+
+  // Fires when Receptionist is moved from one HTML document to another one
+  adoptedCallback() {}
+}
+
+// Register Receptionist as a custom element
+window.customElements.define("saas-receptionist", Receptionist);
